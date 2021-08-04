4ffdd9cf87f7d146b5e98f4db7b1dbbe022ec289
meme8383 <eduard.tanase1@gmail.com>
2021-08-04 16:23:40 -0700

    Final Project

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 client_id = "861456218017890314"
-token = "ODYxNDU2MjE4MDE3ODkwMzE0.YOKDnw.MOLACMaZq9lOteJehRtgyuGsVD8"
+token = "ODYxNDU2MjE4MDE3ODkwMzE0.YOKDnw.BGFgMitCUHVggcP7pyiIKQn0rf0"
 postgres = "btesw"
 postgres_user = "postgres"
 postgres_pass = "password"
