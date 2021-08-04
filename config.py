56a0a35c78d412d4a579bb2a06b4499f4726b048
meme8383 <eduard.tanase1@gmail.com>
2021-08-04 16:16:34 -0700

    Final Project

---
+++ config.py
@@ -0,0 +1,6 @@
+client_id = "861456218017890314"
+token = "ODYxNDU2MjE4MDE3ODkwMzE0.YOKDnw.MOLACMaZq9lOteJehRtgyuGsVD8"
+postgres = "btesw"
+postgres_user = "postgres"
+postgres_pass = "password"
+log_channel_id = 813642660388143124
