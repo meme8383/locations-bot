c193ef2c59a9ff6334f5b99982bc154474e0dd30
dy-dx-1 <nic19sarbe@gmail.com>
2022-02-26 19:32:01 -0500

    Events added

---
+++ cogs/Events.py
@@ -0,0 +1,85 @@
+import discord
+from discord.ext import commands 
+
+class Kgnar_message: 
+    id = 363076151867867144  # Kgnar's id 
+    @staticmethod
+    def swear_filter(msg):  # needs to have a clean msg to analyse when input
+        swears = [
+            'frick',
+            'fuck', 
+            'stupid',
+            'idiot',
+            'idot',
+            'cunt',
+            'fucking'
+        ]
+        for swear in swears: 
+            if swear in msg: return True 
+            else: pass 
+        return False 
+    def __init__(self, content): 
+        self.content = content  # Equivalent to message parameter from Events 
+        self.msg = self.content.lower().strip() # 'clean' version of the message to use in checks 
+    def check_kgnar(self):  # returns true if the author of the message that created the obj is kgnar 
+        return True if self.msg.author.id==self.id else False 
+    def neutral_emoji_check(self): 
+        if ':neutral_face:' in self.msg: return "SMH STOP USING THAT EMOJI STUPID STUPID STUPID :neutral_face:"
+    def grin_emoji_check(self): 
+        if ':grin:' in self.msg: return "YOU ARE NOT FUNNY IDOT STOP SOTP SOPT SPOTSPOTPOSPO"
+    def jesus_check(self): 
+        if 'jesus' in self.msg: return "me (jesus) when kgnar mentions my name :rolling_eyes::rolling_eyes::rolling_eyes:"
+    def lake_tahoe(self):
+        if 'tahoe' in self.msg: return "ewwww kgnar hometown?????? :nauseated_face:"
+    def ew(self): 
+        if ":face_with_raised_eyebrow:" in self.msg: return ":face_with_raised_eyebrow:kgnar spoke:face_with_raised_eyebrow:, must get her manager <@360392861608574978>"
+    def no(self):
+        if "no" and "furry" in self.msg: return "bad furry."
+        elif "no" in self.msg: return "bad kgnar. :thumbsdown: :thumbsdown: :thumbsdown: :thumbsdown: :thumbsdown: :thumbsdown: "
+    def asked(self):
+        if ":rage:" in self.msg: return "nobody asked though :rofl: :rofl: :rofl:"
+    def roll(self):
+        if "rolling_eyes" in self.msg: return "kgnar is misbehaving again, I apologize for this incident. please contact her manager"
+    def cry(self):
+        if ":sob:" in self.msg: return ":joy::joy::joy::joy::joy:"
+    def yawn(self):
+        if ":yawning_face:" in self.msg: return "shut up"
+    def swear(self): 
+        if Kgnar_message.swear_filter(self.msg): return "STOP SWEARING KGNAR YOU DON'T HAVE HUMAN RIGHTS :rage:"
+    kgnar_functions_list = [ # Holds all the functions that we'll loop through, don't forget to add a function here after implementing it
+        neutral_emoji_check, 
+        grin_emoji_check, 
+        jesus_check, 
+        lake_tahoe,
+        ew,
+        no,
+        asked,
+        roll,
+        cry,
+        yawn,
+        swear
+    ]
+
+class Events(commands.Cog): 
+    def __init__(self, bot): 
+        self.client = bot 
+    
+    @commands.Cog.listener()
+    async def on_message(self, message): 
+        if message.author == self.client.user: return # ignore messages from the bot 
+
+        if message.author.id == Kgnar_message.id: # if we enter here, kgnar has sent a message   
+            # creating obj 
+            kgnar = Kgnar_message(message)
+            # first respond with :nauseated_face: 
+            message.channel.send("the ultimate furry :nauseated_face:")
+            # now checking if our functions are met and responding accordingly 
+            for function in kgnar.kgnar_functions_list: # accessing function list of the kgnar obj, could also be the kgnar class 
+                response = function(kgnar) # Running the function passing the kgnar obj cause self is not implicit in this case 
+                if response: 
+                    message.channel.send(response) 
+                else:
+                    pass # if response is none, just continue looping and/or finish the process 
+
+def setup(bot):
+    bot.add_cog(Events(bot))
